f0b03f43518899ccbae388f0b103eb459a72eca4
u31d81 <u31d81@INGURWN130183>
2023-10-31 16:55:48 +0530

    Add project files.

---
+++ Assignment5/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Account
+    {
+        int id;
+        public int Id {  get { return id; } set { id = value; } }
+        string? accountType;
+        public string? AccountType { get {  return accountType; } set {  accountType = value; } }    
+        double balance;
+        public double Balance { get { return balance;} set { balance = value; } }   
+        public bool Withdraw(double amount)
+        {
+            if (balance >=amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+        public string GetDetails()
+        {
+            StringBuilder details = new StringBuilder();
+            details.AppendLine("Account Id: " + Convert.ToString(id));
+            details.AppendLine("Account Type: " + AccountType);
+            details.Append("Balance: " + Convert.ToString(balance));
+            return details.ToString();
+        }
+        
+    }
+}
---
+++ Assignment5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List<Account> accounts = new List<Account>() {
+            //new Account(){Id=0,AccountType="savings",Balance=1000},
+            //new Account(){Id=2,AccountType="savings",Balance=4000},
+            //new Account(){Id=4,AccountType="current",Balance=10000},
+            //new Account(){Id=1,AccountType="savings",Balance=2000},
+            //new Account(){Id=9,AccountType="savings",Balance=10000},
+            //new Account(){Id=8,AccountType="current",Balance=3000},
+            //new Account(){Id=7,AccountType="savings",Balance=12000},
+            //new Account(){Id=6,AccountType="savings",Balance=21000},
+            //new Account(){Id=5,AccountType="current",Balance=51000},
+            //new Account(){Id=3,AccountType="current",Balance=11000}
+            //};
+            //for(int i = 0; i < 2; i++)
+            //{
+            Account accounts = new Account() { Id = 0, AccountType = "savings", Balance = 1000 };
+                string details=accounts.GetDetails();
+                Console.WriteLine(details); 
+                Console.WriteLine("amount to be deducted");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                if (accounts.Withdraw(amount))
+                {
+                    Console.WriteLine("New Balance: " + accounts.Balance);
+                Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Transaction failed! Not enough balance");
+                Console.WriteLine();
+                }
+
+            
+
+        }
+    }
+}
